44f6001a08911036e7e978211ce53697d7098d1b
Candyroom136 <candyroom136@naver.com>
2021-09-07 11:40:27 +0900

    Add getDotProduct.js
    두 개의 백터의 내적을 구하는 문제 풀이

---
+++ getDotProduct.js
@@ -0,0 +1,6 @@
+function getDotProduct(vectorA, vectorB) {
+    return  vectorA.reduce((dotProduct, elementA, indexOfB) => (dotProduct + elementA * vectorB[indexOfB]), 0);
+}
+
+console.log(getDotProduct([1,2,3,4], [-3,-1,0,2])) // 결과값	3
+console.log(getDotProduct([-1,0,1],	[1,0,-1]))     // 결과값	-2
