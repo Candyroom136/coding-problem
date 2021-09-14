1ab3eb6829e24216305a644e0519dfeff8815697
Candyroom136 <candyroom136@naver.com>
2021-09-14 17:17:07 +0900

    Add getCaseNumber.js
    2 * N 크기의 사각형을 2 * 1크기의 사각형, 1 * 2 크기의 사각형, 2 * 2 크기의 사각형으로 채울 수 있는 경우의 수를 구하는 문제

---
+++ getCaseNumber.js
@@ -0,0 +1,28 @@
+const interface = require('readline').createInterface({ 
+    input: process.stdin, 
+    output: process.stdout 
+});
+const input = [];
+interface.on
+(
+  'line',
+  function (line)
+  {
+    input.push(line);
+  }
+).on
+(
+  'close',
+  function ()
+  {
+    console.log(getCaseNumber(Number(input[0])));
+    process.exit();
+    function getCaseNumber(n) {
+        const memo = [1, 1, 3];
+        for (let i = 3; i <= n; i++) {
+            memo[i] = memo[i - 1] + 2*memo[i -2 ];
+        }
+        return memo[n] % 10007;
+    }
+  }
+)
