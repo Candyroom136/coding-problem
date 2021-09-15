8e4fc2ac521b15f39907f3e720a683791a6129af
Candyroom136 <candyroom136@naver.com>
2021-09-15 17:15:01 +0900

    Add islandNumber.js
    섬들을 표시한 지도를 사용자가 0과 1과 스페이스와 엔터를 통해 입력 할 때 해당 내용을 통해 섬의 갯수를 알려주는 문제 풀이

---
+++ islandNumber.js
@@ -0,0 +1,74 @@
+const interface = require('readline').createInterface({ 
+  input: process.stdin, 
+  output: process.stdout 
+});
+const input = [];
+
+interface.on(
+  'line',
+  function (line) {
+    input.push(line);
+  }
+).on(
+  'close',
+  function () {
+
+		while (input.length !== 0 && input[0] !== '0 0') {
+			const currentMap = makeMap();
+			const currentIslandNumber = getIslandNumber(currentMap);
+			console.log(currentIslandNumber);
+		} 
+
+    	process.exit();
+
+		function makeMap() {
+			const [width, height] = 	input.shift().split(' ').map(cur => Number(cur));
+			const map = new Array(height).fill(0).map(cur => new Array(width));
+			
+			for (let i = 0; i < height; i++) {
+				const rowMap = input.shift().split(' ');
+				for (let j = 0; j < width; j++) {
+					map[i][j] = rowMap[j];
+				}
+			}
+
+			return map;
+		}
+
+		function getIslandNumber(map) {
+			const isVisited = new Array(map.length).fill(0).map(cur => new Array(map[0].length).fill(false));
+			let islandNumber = 0;
+
+			for (let i = 0; i < map.length; i++) {
+				for (let j = 0; j < map[0].length; j++) {
+					if (isValidLocation(i, j)) {
+						visitIsland(i, j);
+						islandNumber++;
+					}
+				}
+			}
+
+			return islandNumber;
+	
+			function visitIsland(x, y) {
+				if(isValidLocation(x, y)) {
+					isVisited[x][y] = true;
+					visitIsland(x + 1, y);
+					visitIsland(x - 1, y);
+					visitIsland(x, y + 1);
+					visitIsland(x, y - 1);
+					visitIsland(x + 1, y + 1);
+					visitIsland(x + 1, y - 1);
+					visitIsland(x - 1, y + 1);
+					visitIsland(x - 1, y - 1);
+				} else {
+					return 0;
+				}
+			}
+
+			function isValidLocation(x, y) {
+				return isVisited[x] && !isVisited[x][y] && map[x][y] === '1'
+			}
+		}
+  }
+)
